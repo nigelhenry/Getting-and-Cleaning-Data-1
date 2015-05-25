40c15246b214d8197abe0ac7b4a66297e48d102e
nigelhenry <nigelhenry@gmail.com>
2015-05-25 01:58:48 -0400

    Submission for Peer Assessment

---
+++ README.md
@@ -0,0 +1,17 @@
+---
+title: "Getting and Cleaning Peer Assessment"
+author: "Nigel AR Henry"
+date: "Sunday, May 24, 2015"
+output: html_document
+---
+
+The script in this folder completes the steps in the assignment
+* loads the data
+* merges the data sets
+* labels and names the data sets
+* extracts only the relevant columns
+* makes a summary table
+* exports this table
+
+
+A codebook is also attached.
---
+++ getdata_peerassessment.R
@@ -0,0 +1,35 @@
+## load the data
+dataFolder <- "./getdata-projectfiles-UCI HAR Dataset/UCI HAR Dataset/"
+features <- read.table(paste(dataFolder, 'features.txt', sep=""), stringsAsFactors=F)
+labels <- read.table(paste(dataFolder, 'activity_labels.txt', sep=""), stringsAsFactors=F)
+
+X_train<-read.table(paste(dataFolder, 'test/X_test.txt', sep=""))
+y_train<-read.table(paste(dataFolder, 'test/y_test.txt', sep=""))
+subject_train<-read.table(paste(dataFolder, 'test/subject_test.txt', sep=""))
+X_test<-read.table(paste(dataFolder, 'test/X_test.txt', sep=""))
+y_test<-read.table(paste(dataFolder, 'test/y_test.txt', sep=""))
+subject_test<-read.table(paste(dataFolder, 'test/subject_test.txt', sep=""))
+
+## merge the data sets
+activity <- rbind(
+  cbind(subject_train, y_train, X_train),
+  cbind(subject_test, y_test, X_test)
+)
+
+## label the data
+names(activity) <- c("subject", "activity", features$V2)   # label features
+activity <- merge(activity, labels, by.x="activity", by.y="V1", all.x=T) # merge on activity labels
+names(activity)[ncol(activity)] <- "activity_label"  # name the activity label column
+
+## extracting ONLY means and standard deviations
+activity <- cbind(
+  activity[,c("subject","activity_label")],
+  activity[,(grepl("mean()", names(activity)) | grepl("std()", names(activity)))]
+)
+
+## second data set with means for each activity and each subject
+library(plyr)
+activityMeans <- ddply (activity, .(subject, activity_label), numcolwise(mean))
+
+## output data as text file
+write.table(activityMeans, "activityMeans.txt", row.name=FALSE)
